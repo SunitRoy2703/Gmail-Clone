8d665cd17adc86b6cfbe9b9f2639af70a965ab8e
Sunit Roy <2703iamsry@gmail.com>
2021-04-01 03:01:19 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# gmail_clone
+
+## Screenshots
+<img align="left" src="https://user-images.githubusercontent.com/67560900/113213701-26e19600-9296-11eb-9a1b-ee673df247d3.jpeg" height="500em" />
+<img align="left" src="https://user-images.githubusercontent.com/67560900/113213716-28ab5980-9296-11eb-921c-6d94e61c7c57.jpeg" height="500em" />
+
+
+
+
+
+
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:gmail_clone/ui/home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Gmail Clone',
+      theme: ThemeData(
+        primaryColor: Colors.white,
+        accentColor: Colors.black
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
